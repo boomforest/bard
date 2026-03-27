c18a1ebc9afb00cb1ba9ffd279d488d0b712f980
John Paul Roney <boomforest@Boom-Forest-Church-of-the-Immaculate-Motherboard.local>
2026-03-27 14:48:44 -0600

    QR code encodes ticket number only instead of full URL

--- src/TicketView.jsx
+++ src/TicketView.jsx
@@ -352,7 +352,7 @@ export default function TicketView() {
 
           {/* QR Code */}
           <QRCode
-            value={`${window.location.origin}/t/${ticket?.id}`}
+            value={`${ticket?.ticket_number}`}
             size={160}
             bgColor="#130800"
             fgColor="#d2691e"
